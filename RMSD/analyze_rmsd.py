b9b22a782c40d496e00e8b520934101dd14259d5
jesusdfc <a904346@alumni.unav.es>
2023-06-06 18:43:47 +0200

    rmsd analysis on egfr and gskb finished

--- RMSD/analyze_rmsd.py
+++ RMSD/analyze_rmsd.py
@@ -10,11 +10,11 @@ import seaborn as sns
 ref = pd.read_csv(os.path.join('RMSD','final_fold_biosnap_annotated.tsv'), sep='\t', index_col=0)
 
 ## load --> final fold threshold rmsd 6
-rmsd_files = os.listdir(os.path.join('RMSD','FinalFold_threshold6_5_seeds_BIOSNAP'))
+rmsd_files = sorted(os.listdir(os.path.join('RMSD','FinalFold_threshold6_5_seeds_BIOSNAP')))
 rmsd = {f'seed_{i}': pd.read_csv(os.path.join('RMSD','FinalFold_threshold6_5_seeds_BIOSNAP',x,'predictions.csv'),index_col=0) for i,x in enumerate(rmsd_files)}
 
 ## load --> final fold threshold random
-random_files = os.listdir(os.path.join('RMSD','FinalFold_Random_5_seeds_BIOSNAP'))
+random_files = sorted(os.listdir(os.path.join('RMSD','FinalFold_Random_5_seeds_BIOSNAP')))
 random = {f'seed_{i}': pd.read_csv(os.path.join('RMSD','FinalFold_Random_5_seeds_BIOSNAP', x, 'predictions.csv'),index_col=0) for i,x in enumerate(random_files)}
 
 
@@ -49,4 +49,60 @@ ax.set_title(f'NS (p-val = {p_value})')
 plt.xlabel('Conditions')
 plt.ylabel('Percentage')
 plt.savefig(os.path.join('RMSD','rmsd_random_biosnap_comparison.png'))
+plt.clf()
+plt.cla()
+plt.close()
 
+
+"""
+CHECK SCORE
+
+Gene	Protein_Name	UniProtKB	Drug ID	Drug_Name
+P00533	Epidermal growth factor receptor	EGFR_HUMAN	DB08247	6-(cyclohexylmethoxy)-8-isopropyl-9h-purin-2-amine
+P49841	Glycogen synthase kinase-3 beta	GSK3B_HUMAN	DB00748	carbinoxamine
+"""
+
+## get position of first
+name = 'EGFR'
+p1 = ref[ref['Gene'] == 'P00533']
+p1 = p1[p1['Drug ID'] == 'DB08247'].index[0]
+
+# name = 'GSKB'
+# p1 = ref[ref['Gene'] == 'P49841']
+# p1 = p1[p1['Drug ID'] == 'DB00748'].index[0]
+
+score_rmsd = []
+bool_rmsd = []
+score_random = []
+bool_random = []
+
+for i in range(5):
+    s = f'seed_{i}'
+
+    ## get pred and true
+    rmsd_score = rmsd[s].iloc[p1,0]
+    rmsd_bool = rmsd[s].iloc[p1,1]
+    score_rmsd.append(rmsd_score)
+    bool_rmsd.append(rmsd_bool)
+
+    random_score = random[s].iloc[p1,0]
+    random_bool = random[s].iloc[p1,1]
+    score_random.append(random_score)
+    bool_random.append(random_bool)
+
+
+# perform wilcoxon test
+odds_ratio, p_value = wilcoxon(score_rmsd, score_random)
+
+# Create a DataFrame from the arrays
+data = pd.DataFrame({'RMSD': score_rmsd, 'RANDOM': score_random})
+# Plot boxplots using seaborn
+ax = sns.boxplot(data=data)
+ax.set_title(f'{name}: NS (p-val = {p_value})')
+# Add labels to the plot
+plt.xlabel('Conditions')
+plt.ylabel('Prediction Probability')
+plt.savefig(os.path.join('RMSD',f'rmsd_random_biosnap_comparison_{name}.png'))
+plt.clf()
+plt.cla()
+plt.close()
